f98716790d8c008e6264a60b59fcb36b0c8ba567
‘Gonzo <gonzonieto@gmail.com>
2022-03-17 02:14:38 +0000

    refactored client connection code

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
-const net = require('net');
+const {connect} = require('./client');
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // host IP address
-    port: '50541'
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  // receive incoming data and log it to console
-  conn.on('data', (data) => {
-    console.log(data.toString())
-  });
-  return conn;
-};
 
 console.log("Connecting ...");
 
